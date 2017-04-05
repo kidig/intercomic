0493b6f993dca61185823a938d4a1c698bb738dc
kidig <kiddima@gmail.com>
2017-04-05 23:29:51 +0800

    Initial library

---
+++ intercomic/__init__.py
@@ -0,0 +1,20 @@
+from .client import Client
+from requests.auth import AuthBase
+
+
+__all__ = ['IntercomApp']
+
+
+class IntercomAuth(AuthBase):
+    def __init__(self, token):
+        self.token = token
+
+    def __call__(self, r):
+        r.headers['Authorization'] = 'Bearer {}'.format(self.token)
+        return r
+
+
+class IntercomApp(Client):
+    def __init__(self, access_token, *args, **kwargs):
+        auth = IntercomAuth(access_token)
+        super(IntercomApp, self).__init__(auth=auth, *args, **kwargs)
